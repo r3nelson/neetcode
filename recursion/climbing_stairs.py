2cd65c31c5c478a6b2662ae3ce9c472968006096
Richard Nelson <r3nelson@outlook.com>
2024-09-29 19:16:00 -1000

    completed climbing stairs

--- recursion/climbing_stairs.py
+++ recursion/climbing_stairs.py
@@ -1,12 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        # one step
-        if n == 0:
-            return 
+        l,r = 1, 1
 
-        # two steps
-
-
-print(5/2)
-
+        for i in range(n):
+            temp = r
+            r = l + r
+            l = temp
+        
+        return r
+    
+        # # recursive solution
+        # if n == 1:
+        #     return 1
+        
+        # if n == 2:
+        #     return 2
+        # return self.climbStairs(n-1) + self.climbStairs(n-2)
