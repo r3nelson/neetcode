df65cd653a6c01df4a74c87aeb3366cfd62389ae
Richard Nelson <r3nelson@outlook.com>
2024-10-02 21:21:44 -1000

    completed merge two sorted linked lists

---
+++ linkedLists/merge two sorted linked lists.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import Optional
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+
+        dummy = ListNode()
+        tail = dummy
+
+        # compare values of two linked lists
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+                
+            elif list2.val < list1.val:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+            tail.next = None
+                
+        # get the rest of the values in the list
+        if list1:
+            tail.next = list1
+            
+        if list2:
+            tail.next = list2
+
+        return dummy.next
+
